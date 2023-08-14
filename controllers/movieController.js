5133f2c3a6a5a0b90a2358a8c1be91bf93114558
Matheus Fillipe <mfsm@cin.ufpe.br>
2023-08-14 12:22:35 -0300

    adding method patch

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,5 +1,6 @@
 const Movie = require('../models/Movie')
 
+// Read Movies
 exports.getAllMovies = async (req, res) => {
 
     try{
@@ -16,6 +17,7 @@ exports.getAllMovies = async (req, res) => {
     }
 }
 
+// Create Movie
 exports.createMovie = async (req, res) => {
 
     try{
@@ -39,11 +41,12 @@ exports.createMovie = async (req, res) => {
     }
 }
 
+// Delete Movie
 exports.deleteMovie = async (req, res) => {
 
     try{
         await Movie.findByIdAndDelete(req.params.id)
-        res.status(204).json({
+        res.status(200).json({
             status: 'Ok',
             data: null
         })
@@ -52,5 +55,34 @@ exports.deleteMovie = async (req, res) => {
             status: 'fail',
             message: `ERROR => ${err}`
         })
+    }
+}
+
+// Update Movie
+exports.updateMovie = async (req, res) => {
+
+    try{
+        const updatedMovie = await Movie.updateOne(
+            { _id: req.params.id },
+            {
+                $set: {
+                    movieName: req.body.movieName,
+                    movieRating: req.body.movieRating,
+                    releaseDate: req.body.releaseDate,
+                    directorName: req.body.directorName
+                }
+            }
+        )
+        res.status(200).json({
+            status: 'Ok',
+            data: { 
+                movie: updatedMovie
+            }
+        })
+    }catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: `ERROR => ${err}`
+        })     
     }
 }
--- router/movieRoute.js
+++ router/movieRoute.js
@@ -6,6 +6,6 @@ const router = express.Router()
 router.route('/').get(movieController.getAllMovies)
 router.route('/').post(movieController.createMovie)
 router.route('/:id').delete(movieController.deleteMovie)
-
+router.route('/:id').patch(movieController.updateMovie)
 
 module.exports = router
