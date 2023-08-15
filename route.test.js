36dbb2233fcaf86f7122edbf572fa815511e2ba4
Matheus Fillipe <mfsm@cin.ufpe.br>
2023-08-14 23:02:34 -0300

    adding automated tests with jest

---
+++ route.test.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const app = require('./index');
+
+let server;
+let serverPort;
+
+describe('Test my app', () => {
+  beforeAll(done => {
+    server = app.listen(0, () => {
+      serverPort = 3000;
+      done();
+    }); // Inicia o servidor
+  });
+
+  afterAll(done => {
+    server.close(() => {
+      done();
+    }); // Fecha o servidor após todos os testes
+  });
+
+  it('should get all movies', async () => {
+    const response = await axios.get(`http://localhost:${serverPort}/api/movies`);
+
+    expect(response.status).toBe(200);
+    expect(response.data.status).toBe('Ok');
+    console.log(response.data.data.movies);
+  });
+});
