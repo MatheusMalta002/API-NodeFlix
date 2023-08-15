115ee591e230a4c89b1e4062493b9d38a4f6f2d5
Matheus Fillipe <mfsm@cin.ufpe.br>
2023-08-15 10:22:58 -0300

    adding post test

--- route.test.js
+++ route.test.js
@@ -3,6 +3,7 @@ const app = require('./server');
 
 describe('Test my app', () => {
 
+  //GET
   test('should get all movies', async () => {
     const response = await request(app).get('/api/movies')
 
@@ -11,6 +12,24 @@ describe('Test my app', () => {
     expect(Array.isArray(response.body.data.movies)).toBe(true);
   });
 
+  //POST
+  test('should create a new movie', async () => {
+    const newMovie = {
+      movieName: 'Test Movie 2',
+      movieRating: 8.5,
+      releaseDate: '2023-01-01', 
+      directorName: 'Test Director',
+    };
+
+    const res = await request(app).post('/api/movies').send(newMovie);
+
+    expect(res.status).toBe(201);
+    expect(res.body.status).toBe('Ok');
+  }); 
+
+  //PACTH
+
+  //DELETE
 
 });
 
